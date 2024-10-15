79675e885601685456765efd2cc7961fb2a521ea
Fran Bec <franbecvort@gmail.com>
2024-10-15 11:24:31 +0100

    Improved @Mapper

--- src/main/java/dev/pollito/post/mapper/UserMapper.java
+++ src/main/java/dev/pollito/post/mapper/UserMapper.java
@@ -3,8 +3,9 @@ package dev.pollito.post.mapper;
 import dev.pollito.post.model.User;
 import java.util.List;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface UserMapper {
   List<User> map(List<com.typicode.jsonplaceholder.model.User> users);
 }
